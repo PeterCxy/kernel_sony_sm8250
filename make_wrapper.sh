98af84353e5d2f13ad992eadef712b327c4927d1
Peter Cai <peter@typeblog.net>
2022-05-31 11:14:31 -0400

    build: refactor: use symlink to define ANDROID_ROOT
    and move the build directory inside the kernel source tree (out/)

--- make_wrapper.sh
+++ make_wrapper.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 if [ -z "$ANDROID_ROOT" ]; then
+  ANDROID_ROOT=$(readlink build/android-root)
+fi
+
+if [ -z "$ANDROID_ROOT" ]; then
+  echo "ANDROID_ROOT undefined"
   exit 1
 fi
 
+mkdir -p out
+
 export PATH=$ANDROID_ROOT/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:$ANDROID_ROOT/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin:$ANDROID_ROOT/prebuilts/clang/host/linux-x86/clang-r416183b/bin/:$PATH
 
-exec make O=$PWD/../edo-4.19-out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi- CC=$ANDROID_ROOT/prebuilts/clang/host/linux-x86/clang-r416183b/bin/clang CLANG_TRIPLE=aarch64-linux-gnu -j12 $@
+exec make O=$PWD/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi- CC=$ANDROID_ROOT/prebuilts/clang/host/linux-x86/clang-r416183b/bin/clang CLANG_TRIPLE=aarch64-linux-gnu -j12 $@
