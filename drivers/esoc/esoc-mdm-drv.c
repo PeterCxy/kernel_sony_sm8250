dd8776823ee57974804a199d6fc11187188bcced
Peter Cai <peter@typeblog.net>
2023-01-22 15:36:14 -0500

    [DNM] drivers: esoc: Haxx: Do not panic if external modem failed to boot

--- drivers/esoc/esoc-mdm-drv.c
+++ drivers/esoc/esoc-mdm-drv.c
@@ -419,9 +419,9 @@ static int mdm_handle_boot_fail(struct esoc_clink *esoc_clink, u8 *pon_trial)
 		msleep(S3_RESET_DELAY_MS);
 		break;
 	case BOOT_FAIL_ACTION_PANIC:
-		esoc_mdm_log("Calling panic!!\n");
-		panic("Panic requested on external modem boot failure\n");
-		break;
+		//esoc_mdm_log("Calling panic!!\n");
+		//panic("Panic requested on external modem boot failure\n");
+		//break;
 	case BOOT_FAIL_ACTION_NOP:
 		esoc_mdm_log("Leaving the modem in its curent state\n");
 		mdm_drv->mode = PWR_OFF;
